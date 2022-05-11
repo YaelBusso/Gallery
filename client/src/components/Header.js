c6dddde02459003985c7ccaccb7692b106c8b10c
YaelBusso <y0583239076@gmail.com>
2022-05-11 09:35:45 +0300

    add icons

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {Navbar, Container, Nav, NavDropdown, } from "react-bootstrap";
+import Icon from './Icon'
 
 const Header = () => {
   return (
@@ -10,26 +11,15 @@ const Header = () => {
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
+        
             </Nav>
             <Nav>
-              <Nav.Link href="#deets">More deets</Nav.Link>
-              <Nav.Link eventKey={2} href="#memes">
-                source code
+            <Nav.Link href="#deets"><Icon name="lightbox"/></Nav.Link>
+              <Nav.Link href="#deets"><Icon name="row"/></Nav.Link>
+              <Nav.Link href="#deets"><Icon name="col"/></Nav.Link>
+              <Nav.Link href="#deets"><Icon name="eye"/></Nav.Link>
+              <Nav.Link eventKey={2} href="https://github.com/YaelShrem/Gallery">
+                <Icon name="githubLogo" />
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
