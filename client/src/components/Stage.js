b11437a922710cb95de350ca49045b2626b91332
YaelBusso <y0583239076@gmail.com>
2022-05-11 19:11:29 +0300

    fixes

--- client/src/components/Stage.js
+++ client/src/components/Stage.js
@@ -1,6 +1,9 @@
 import React, {useEffect} from 'react'
 import GridGallery from './GridGallery'
 import { useSelector, useDispatch } from "react-redux";
+import * as api from "../api/index.js";
+import { getImages } from "../features/imagesSlice";
+
 function Stage() {
         const dispatch = useDispatch();
         useEffect(() => {
